ca7a871e23d950447bdb558b5210ae0dccdd0e19
Soohyun <csh0sns@naver.com>
2024-01-17 00:01:53 -0500

    feat: Add link to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
     return (
         <div className="Header">
             <div id="logo">Soohyun Choi</div>
             <div id="menu">
-                <div className="menu-item">Home</div>
-                <div className="menu-item">Projects</div>
-                <div className="menu-item">Resume</div>
-                <div className="menu-item">Contact</div>
+                <NavLink className="menu-item" to="/">Home</NavLink>
+                <NavLink className="menu-item" to="/projects">Projects</NavLink>
+                <NavLink className="menu-item" to="/resume">Resume</NavLink>
+                <NavLink className="menu-item" to="/contact">Contact</NavLink>
             </div>
         </div>
     );
