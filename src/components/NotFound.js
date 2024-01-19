97ef91c55ba6cd5fa15afadf2b05f2f02d1ccccb
Soohyun <csh0sns@naver.com>
2024-01-19 17:25:22 -0500

    feat: Update 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,17 @@
+import React from "react";
+import "../styles/NotFound.scss";
+
 function NotFound() {
+    const notFound = require("../assets/404NotFound.png");
+
     return (
-      <div className="NotFound">
-          404 Not Found
-      </div>
+        <div className="NotFound">
+            <div>
+                <span className="blue">OOPS! </span> <span>PAGE NOT FOUND</span>
+            </div>
+            <img src={ notFound } alt="404 Not Found"/>
+            <div><a href="https://storyset.com/online">Online illustrations by Storyset</a></div>
+        </div>
     );
 }
 
