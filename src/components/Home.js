c9d872c199c11c78df71783567c2cd50c1d40fa5
Soohyun <csh0sns@naver.com>
2024-01-22 17:43:17 -0500

    feat: Add icon to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/Home.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {Link} from "react-router-dom";
 
 function Home() {
     const profile = require("../assets/profile.png");
@@ -9,6 +12,15 @@ function Home() {
             <div className="left-side">
                 <div className="subtitle">Hello, I'm</div>
                 <div className="title">Soohyun<br />Choi</div>
+                <div className="links">
+                    <Link to="https://github.com/Soohyun-CHOI">
+                        <FontAwesomeIcon icon={ faGithub } size="3x" />
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/soohyn-choi/">
+                        <FontAwesomeIcon icon={ faLinkedin } size="3x" />
+                    </Link>
+                    <button className="resume">Resume</button>
+                </div>
             </div>
                 <img src={ profile } alt="profile" />
         </div>
