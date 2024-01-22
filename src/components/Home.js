ac2596c734ae6a1b851309cbe6bb75432cc1819d
Soohyun <csh0sns@naver.com>
2024-01-21 19:17:23 -0500

    feat: Update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import React from "react";
+import "../styles/Home.scss";
+
 function Home() {
+    const profile = require("../assets/profile.png");
+
     return (
         <div className="Home">
-            home
+            <div className="left-side">
+                <div className="subtitle">Hello, I'm</div>
+                <div className="title">Soohyun<br />Choi</div>
+            </div>
+                <img src={ profile } alt="profile" />
         </div>
     );
 }
